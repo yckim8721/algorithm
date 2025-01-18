353d087b05e11a8205472fc214418c1d4227feb0
Youngchan Kim <yckim8721@gmail.com>
2025-01-18 23:48:26 +0900

    Update ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -9,25 +9,25 @@ class Solution {
             }
             t += (long)times[i];
         }
-        
         return limit < t;
     }
-    /*
-    이진탐색을 활용, mid값을 level로 설정
-    */
+    
+    // 이진탐색을 활용, mid값을 level로 설정
     public int solution(int[] diffs, int[] times, long limit) {
-        //diff: 난이도, time_cur: 소요시간, time_prev: 이전 소요시간, level: 숙련도
-        //diff<=level time_cur만큼 사용
-        //diff>level diff-level번 틀림 틀릴때 마다 time_cur 사용, 추가로 time_prev 시간을 사용해 이전 퍼즐 풀고 와야함 이 때는 난이도 상관없이 틀리지 않음, diff - level번 틀린 후에 다시 풀면 time_cur 시간 사용하여 해결
+        // diff: 난이도, time_cur: 소요시간, time_prev: 이전 소요시간, level: 숙련도
+        // diff <= level time_cur만큼 사용
+        // diff > level diff-level번 틀림 틀릴때 마다 time_cur 사용,
+        // 추가로 time_prev 시간을 사용해 이전 퍼즐 풀고 와야함 이 때는 난이도 상관없이 틀리지 않음
+        // diff - level번 틀린 후에 다시 풀면 time_cur 시간 사용하여 해결
         long left = 1;
         long right = limit;
         
         while(left < right){
             long mid = (left + right) / 2;
             
-            if(isImpossible(diffs, times, mid, limit)){
+            if (isImpossible(diffs, times, mid, limit)) {
                 left = mid + 1;
-            }else{
+            } else {
                 right = mid;
             }
         }
